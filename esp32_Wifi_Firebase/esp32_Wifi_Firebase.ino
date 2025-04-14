72b442c1e81afc1aa8b81b6a1b38755e6344e81b
yassershahofficial <geniususer4u@gmail.com>
2025-04-14 22:34:59 +0800

    connect wifi & firebase, push and retrieve data from firebase

--- esp32_Wifi_Firebase/esp32_Wifi_Firebase.ino
+++ esp32_Wifi_Firebase/esp32_Wifi_Firebase.ino
@@ -1,10 +1,16 @@
 #include <WiFi.h>
+#include <Firebase.h>
+#include <ArduinoJson.h>
 
 //wifi credentials
 #define WIFI_ID "id"
 #define WIFI_PW "password"
+#define FIREBASE_URL "https://firebase_url/"
 
-void setup() {
+//Firebase object - Test Mode
+Firebase fb(FIREBASE_URL);
+
+void setup() {  
   Serial.begin(115200);
   int counter = 0;
   delay(1000);
@@ -15,18 +21,60 @@ void setup() {
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     counter++;
-    delay(300);
+    delay(500);
 
     if(counter > 20){
       Serial.println("\nFailed to connect Wi-Fi");
       return;
     }
   }
-  Serial.println("Wi-Fi is connected!");
+  Serial.println("\nWi-Fi is connected!");
   Serial.print("WiFi Status: ");
   Serial.println(WiFi.status());
-}
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
+  //Create JSON document to record Plant1
+  JsonDocument Plant1;
+
+  Plant1["Status"] = "Connected";
+  Plant1["Temperature"] = 28.4;
+  Plant1["Humidity"] = 34.2;
+
+  Serial.println("Sending data to Firebase.");
+  String Serialized_JSON;
+  serializeJson(Plant1, Serialized_JSON);
+  fb.setJson("Plant1", Serialized_JSON);
+  Serial.println("Data sent to Firebase.");
+
+  Serial.println("Retrieving data from Firebase.");
+  String Retrieved_Serialized_JSON =  fb.getJson("Plant1");
+  JsonDocument Retrieved_Plant1;
+  deserializeJson(Retrieved_Plant1, Retrieved_Serialized_JSON);
+  
+  String Status = Retrieved_Plant1["Status"];
+  float Temperature = Retrieved_Plant1["Temperature"];
+  float Humidity = Retrieved_Plant1["Humidity"];
+  Serial.println("Data retrieved from Firebase.");
+
+  Serial.println("Status : " + String(Status));
+  Serial.println("Temperature : " + String(Temperature));
+  Serial.println("Humidity : " + String(Humidity));
+}
 void loop() {
-
+  // Required even if empty
 }
+
+/*
+  fb.setString("Plant1/Status","Connected"); 
+  fb.setFloat("Plant1/Data/Temperature",28.6);
+  Serial.println("Data sent to Firebase.");
+
+  Serial.println("Fetching Data");
+  String Status = fb.getString("Plant1/Status");
+  float Temperature = fb.getFloat("Plant1/Data/Temperature");
+  Serial.println("Data Fetched");
+
+  Serial.println("Status : " + String(Status));
+  Serial.println("Temperature : " + String(Temperature));
+ */
